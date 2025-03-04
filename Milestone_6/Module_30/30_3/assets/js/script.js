5133618e13e3f7173f72d4e6f7ed81fb3588e05a
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-03-05 00:22:27 +0600

    template string

---
+++ Milestone_6/Module_30/30_3/assets/js/script.js
@@ -0,0 +1,6 @@
+const anthem = `Amar sonar bangla ami tomay`
+const anthem2 ='valobashi';
+console.log(anthem+anthem2)
+
+const fullAnthem= `Amar sonar bangla ami tomay ${anthem2}`;
+console.log(fullAnthem)
