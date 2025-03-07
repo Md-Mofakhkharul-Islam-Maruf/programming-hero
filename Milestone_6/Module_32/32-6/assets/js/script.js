cb8ccd91634899b69117e4dd5b1a737b2e7038aa
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-03-07 22:45:53 +0600

    callback function

--- Milestone_6/Module_32/32-6/assets/js/script.js
+++ Milestone_6/Module_32/32-6/assets/js/script.js
@@ -1,16 +1,14 @@
-// primitive data types contains value not address
-let name = 'Maruf';
-let newName= name;
-console.log(newName)
-
-name= 'Hasan';
-console.log(newName)   
-
-
-// Non-primitive data types contains address not value that's why it changable
-let name2 = { fname: 'Maruf', lname: 'Hasan'};
-let newName2= name2;
-console.log(newName2)
-
-name2.fname= 'Mehedi';
-console.log(newName2)   
+//Callback function 
+function ghotok(patroNo, patroName){
+    patroNo(patroName)    
+}
+function patro1(patroName){
+    console.log(`Ami no 1 patro ${patroName}`);
+}
+function patro2(patroName){
+    console.log(`Ami no 2 patro ${patroName}`);
+}
+function patro3(patroName){
+    console.log(`Ami no 3 patro ${patroName}`);
+}
+ghotok(patro1,'Maruf'); // Here patro1 function passes as an argument of ghotok function
