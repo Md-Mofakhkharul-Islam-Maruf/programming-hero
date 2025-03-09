50584a18634e52bfa19b4d00da1bb3285629e4d4
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-03-10 02:24:42 +0600

    Conceptual session 6

--- Conceptual_session/Milestone_6/all_in_1/script.js
+++ Conceptual_session/Milestone_6/all_in_1/script.js
@@ -88,3 +88,100 @@ const rest = (...take) => {
 }
 rest(1, 5, 6, 78, 9, 2);
 rest(3, 22, 555);
+
+//Array destructing
+const className = [5, 8, 9, 3, 7];
+const [first, , third, gaddu, laddu] = className;
+console.log(gaddu);
+
+// Object destructing
+const objName = { a: 'Maruf', b: 20 }
+const { a, b } = objName;
+console.log(b)
+
+//Object property and method
+const school = {
+    name: 'Lalmonirhat high school', // object property and value
+    location: 'Lalmorinhat',
+    message: function () {
+        return ('Amra school jai'); //object method 
+    }
+
+}
+console.log(school.message());
+
+
+// Objects key value entries loop
+
+const newobj = { name: 'karim', age: 31 };
+for (const key in newobj) {
+    console.log(key)
+}
+
+for (const key in newobj) {
+    console.log(newobj[key])
+}
+
+const entries = Object.entries(newobj);
+console.log(entries)
+
+// Array map
+const qube = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const map = qube.map(store => store * store * store);
+console.log(map);
+
+// Array filter
+const aguntok = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const evenNumber = aguntok.filter(store => store % 2 === 0);
+console.log(evenNumber);
+
+// Array find
+const aguntokNa = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const firstevenNumber = aguntokNa.find(store => store % 2 === 0);
+console.log(firstevenNumber);
+
+// Array of object filter
+const products = [
+    { name: "Apple", price: 20, origin: "USA" },
+    { name: "Banana", price: 5, origin: "Ecuador" },
+    { name: "Mango", price: 30, origin: "India" },
+    { name: "Orange", price: 15, origin: "Spain" },
+    { name: "Grapes", price: 20, origin: "Italy" }
+];
+
+console.log(products.filter(store => store.price === 20))
+
+
+//Foreach
+const furniture = ['Chair', 'table', 'khat'];
+furniture.forEach(element => console.log(element));
+
+//Reduce
+const newArray = [[1, 2], [3, 4], [5, 6]];
+const singleArray = newArray.reduce((acc, current) => {
+    return acc.concat(current);
+}, [])
+console.log(singleArray)
+
+//ternary , && , ?? is easy;
+const n = null;
+const myName = 'Maruf';
+const check= n?? myName;
+console.log(check)
+
+
+//Optional chaining
+const user = {
+    name: "Alice",
+    address: {
+      city: "Wonderland"
+    }
+  };
+  
+  console.log(user?.address?.city); // Output: "Wonderland"
+  console.log(user?.contact?.phone); // Output: undefined
+
+
+  //this
+
+  //clouser
