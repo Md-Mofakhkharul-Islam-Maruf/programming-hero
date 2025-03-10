99f98e0ece0f6007919b93a6ca4cacc0467987a4
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-03-10 23:38:44 +0600

    Load posts and display on the website with CSS

--- Milestone_6/Module_33/33_5/assets/js/script.js
+++ Milestone_6/Module_33/33_5/assets/js/script.js
@@ -1,17 +1,21 @@
 const loadUser = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/posts#')
         .then(response => response.json())
         .then(json => {
-            console.log(json);
-            collectUsers(json);
+            createPost(json)
         });
 }
-const collectUsers = (take) => {
-    const ul= document.getElementById('listed-items');
+loadUser();
+const createPost = (take) => {
+    const body = document.getElementById('container')
     for (let element of take) {
-        // console.log(element.name)
-        const li= document.createElement('li');
-        li.innerText=element.name;
-        ul.appendChild(li);        
+        const div = document.createElement('div');
+        div.classList.add('card')
+        div.innerHTML = `
+            <h1>${element.title}</h1>
+            <h3>${element.id}</h3>
+            <button>Not clickable</button>
+        `;
+        body.appendChild(div);
     }
 }
