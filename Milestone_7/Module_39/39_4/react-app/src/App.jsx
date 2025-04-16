26f3f64c1db4916bba44778f2b8c8a184b9e5750
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-16 07:06:57 +0600

    full overview of useState

---
+++ Milestone_7/Module_39/39_4/react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Batsman from './Batsman'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Batsman></Batsman>
+    </>
+  )
+}
+
+export default App
