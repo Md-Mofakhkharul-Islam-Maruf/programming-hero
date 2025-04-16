2cfe3825842223e39cc2018ef9830fd346d2d9a6
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-16 08:33:50 +0600

    not solve

--- Milestone_7/Module_39/39_6/react-app/src/App.jsx
+++ Milestone_7/Module_39/39_6/react-app/src/App.jsx
@@ -6,23 +6,23 @@ import Friends from './friends'
 const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
   .then(res => res.json())
 
-const fetchFriends = async() => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  return res.json()
-}
+// const fetchFriends = async() => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//   return res.json()
+// }
 
 function App() {
-const friends = fetchFriends();
+// const friends = fetchFriends();
   return (
     <>
       <h1>Vite + React</h1>
       <Suspense fallback={<h3>Amar sonar Bangla</h3>}>
         <Users fetchUser={fetchUsers}></Users>
       </Suspense>
-
+{/* 
       <Suspense fallback={<h3>Friends are comming for treats</h3>}>
         <Friends friends={friends}></Friends>
-      </Suspense>
+      </Suspense> */}
     </>
   )
 }
