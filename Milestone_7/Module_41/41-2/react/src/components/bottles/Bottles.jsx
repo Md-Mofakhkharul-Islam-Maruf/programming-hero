9ffca0feba8c4bad3ceea18a0384fff6cdfddf9b
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-22 18:27:37 +0600

    fetch api

---
+++ Milestone_7/Module_41/41-2/react/src/components/bottles/Bottles.jsx
@@ -0,0 +1,13 @@
+import React, { use } from 'react';
+
+const Bottles = ({bottlesPromise}) => {
+    const bottle = use(bottlesPromise)
+    console.log(bottle)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Bottles;
