a58fed0b71d86832d36ff3370f1241e7ce4d0d47
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-26 15:33:58 +0600

    42-2 Create Fake Data for the blogs and load data

--- Milestone_7/Module_42/my-blogs/src/App.jsx
+++ Milestone_7/Module_42/my-blogs/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Blogs></Blogs>
     </>
   )
 }
