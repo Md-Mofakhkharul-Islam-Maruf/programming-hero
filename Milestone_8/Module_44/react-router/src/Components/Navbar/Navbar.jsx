703d96a94774da5c951563043b9cdaed9eebfcac
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-28 12:08:57 +0600

    use useState for menu icon that means toggle

--- Milestone_8/Module_44/react-router/src/Components/Navbar/Navbar.jsx
+++ Milestone_8/Module_44/react-router/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { CarFront } from 'lucide-react';
+import React, { useState } from 'react';
+import { CarFront, Menu, X } from 'lucide-react';
 
 const Navbar = () => {
     const navigationData = [
@@ -8,20 +8,28 @@ const Navbar = () => {
         { id: 3, name: "Services", path: "/services" },
         { id: 4, name: "Contact", path: "/contact" }
     ];
+
+    const [toggle, setToggle] = useState(true)
     return (
         <div>
-            <ul className='flex justify-center gap-20'>
-                {
-                    navigationData.map(route =>
-                        <li key={route.id}>
-                            <a href={route.path} >{route.name}</a>
-                        </li>
-                    )
-                }
-            </ul>
-            <div>
-                {/* <CarFront /> */}
-            </div>
+            <nav className='flex justify-between'>
+                <span className='flex gap-2' onClick={() => setToggle(!toggle)}>
+                    {
+                        toggle?<Menu/>: <X />
+                    }
+                    <h3>Mynav</h3>
+                </span>
+                <ul className='flex justify-center gap-20'>
+                    {
+                        navigationData.map(route =>
+                            <li key={route.id}>
+                                <a href={route.path} >{route.name}</a>
+                            </li>
+                        )
+                    }
+                </ul>
+                <h3>Sign In</h3>
+            </nav>
         </div>
     );
 };
