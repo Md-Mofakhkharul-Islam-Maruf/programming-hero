000fd54c684f4cc08bd632603d43ec9ae52eac32
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-28 11:17:15 +0600

    Custom nav create using .map

---
+++ Milestone_8/Module_44/react-router/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Camera } from 'lucide-react';
+
+const Navbar = () => {
+    const navigationData = [
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "About", path: "/about" },
+        { id: 3, name: "Services", path: "/services" },
+        { id: 4, name: "Contact", path: "/contact" }
+    ];
+    return (
+        <div>
+            <ul className='flex justify-center gap-20'>
+                {
+                    navigationData.map(route =>
+                        <li key={route.id}>
+                            <a href={route.path} >{route.name}</a>
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
