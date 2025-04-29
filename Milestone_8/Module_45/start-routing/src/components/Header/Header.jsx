282397363c6abcd78e741728b1a91fc54128ecf6
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-29 14:04:02 +0600

    Added sidebar

--- Milestone_8/Module_45/start-routing/src/components/Header/Header.jsx
+++ Milestone_8/Module_45/start-routing/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h2>This is header</h2>
+            <nav>
+                <Link to='/home'>Home</Link>
+                <Link to='/mobiles'>Mobiles</Link>
+                <Link to='/laptops'>Laptops</Link>
+            </nav>
         </div>
     );
 };
--- Milestone_8/Module_45/start-routing/src/components/root/Root.jsx
+++ Milestone_8/Module_45/start-routing/src/components/root/Root.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Header from '../Header/header';
 import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
+import Aside from '../Aside/Aside';
+import './Root.css'
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            <div className='root-main'>
+                <Aside></Aside>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
