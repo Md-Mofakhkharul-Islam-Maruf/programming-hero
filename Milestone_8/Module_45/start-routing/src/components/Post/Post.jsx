c749d029ced65a8d5a088809670934fc39a05ed6
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-04-30 17:33:19 +0600

    use Navigate and use Navigator

--- Milestone_8/Module_45/start-routing/src/components/Post/Post.jsx
+++ Milestone_8/Module_45/start-routing/src/components/Post/Post.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Navigate, useNavigate } from 'react-router';
 import { Link } from 'react-router';
 
 const Post = ({ post }) => {
     const { id, title } = post;
+    const navigate =useNavigate()
+    const handleNavigate = () => {
+        navigate('/mobiles')
+    }
     return (
         <div style={{ border: '2px solid green', margin: '5px' }}>
             <h3>{title}</h3>
             <Link to={`/posts/${id}`}>
                 <button>Show details</button>
             </Link>
+            <button onClick={handleNavigate}>Details of: {id}</button>
         </div>
     );
 };
--- Milestone_8/Module_45/start-routing/src/components/root/Root.jsx
+++ Milestone_8/Module_45/start-routing/src/components/root/Root.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from '../Header/header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 import Aside from '../Aside/Aside';
 import './Root.css'
 
 const Root = () => {
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
     return (
         <div>
             <Header></Header>
             <div className='root-main'>
                 <Aside></Aside>
+                {isNavigating && <span>Loading............</span>}
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
