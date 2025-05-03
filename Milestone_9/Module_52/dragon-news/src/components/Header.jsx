6164720524d6bd4440ebb114749572da8e10e2b5
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-03 17:45:06 +0600

    Added font family to the index.css

--- Milestone_9/Module_52/dragon-news/src/components/Header.jsx
+++ Milestone_9/Module_52/dragon-news/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3 my-10'>
             <img className='w-[400px]' src={logo}/>
-            <p>Journalism Without Fear or Favout</p>
+            <p className='text-accent'>Journalism Without Fear or Favout</p>
         </div>
     );
 };
