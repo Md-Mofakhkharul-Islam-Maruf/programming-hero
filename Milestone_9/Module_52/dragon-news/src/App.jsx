945f15cb69a7acd870240ed2f6748c69762f81cd
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-03 17:30:16 +0600

    Added logo and text in header

--- Milestone_9/Module_52/dragon-news/src/App.jsx
+++ Milestone_9/Module_52/dragon-news/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 
-function App() {(0)
+function App() {
 
   return (
     <>
-    <h1 className='text-7xl text-center'>Lets build the dragon news</h1>
+      <h1 className='text-7xl text-center'>Lets build the dragon news</h1>
     </>
   )
 }
---
+++ Milestone_9/Module_52/dragon-news/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+
+const Header = () => {
+    return (
+        <div className='flex justify-center flex-col items-center gap-3 my-10'>
+            <img className='w-[400px]' src={logo}/>
+            <p>Journalism Without Fear or Favout</p>
+        </div>
+    );
+};
+
+export default Header;
