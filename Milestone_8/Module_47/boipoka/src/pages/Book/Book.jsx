c769ba5f251b697f3d163525bffa58d0bc6fc98a
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-06 16:36:28 +0600

    add extra route

--- Milestone_8/Module_47/boipoka/src/pages/Book/Book.jsx
+++ Milestone_8/Module_47/boipoka/src/pages/Book/Book.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
 import { FaStarHalfStroke } from "react-icons/fa6";
+import { Link } from 'react-router';
 
 
 const Book = ({ singlebook }) => {
     console.log(singlebook);
-    const { image, author, bookId, bookName, publisher, review, totalPages, yearOfPublishing, rating, category } = singlebook
+    const { image, author, bookId, bookName, publisher, review, totalPages, yearOfPublishing, rating, category, tags } = singlebook
     return (
-        <div>
-            <div className="card bg-base-200  shadow-sm border">
-                <figure className='p-8 bg-gray-200 w-2/3 mx-auto rounded-xl my-3'>
-                    <img className=' h-[170px]'
-                        src={image}
-                        alt="Book" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        {bookName}
-                        <div className="badge badge-secondary p-4">{author}</div>
-                    </h2>
-                    <p>A book title plays a crucial role in setting the tone and expectations for the reader. To Kill a Mockingbird suggests a deeper</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">{category}</div>
-                        <div className="badge badge-outline">{rating}<FaStarHalfStroke /></div>
+        <Link to={`bookDetails/${bookId}`}>
+            <div>
+                <div className="card bg-base-200  shadow-sm border">
+                    <figure className='p-8 bg-gray-200 w-2/3 mx-auto rounded-xl my-3'>
+                        <img className=' h-[170px]'
+                            src={image}
+                            alt="Book" />
+                    </figure>
+                    <div className='flex justify-around font-semibold text-blue-400'>
+                        {
+                            tags.map(btn => <p>{btn}</p>)
+                        }
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title flex justify-between">
+                            {bookName}
+                            <div className="badge badge-secondary">{yearOfPublishing}</div>
+                        </h2>
+                        <p>Booked by: {publisher}</p>
+                        <div className='border-t-2 border-dotted'></div>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">{category}</div>
+                            <div className="badge badge-outline">{rating}<FaStarHalfStroke /></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link >
     );
 };
 
