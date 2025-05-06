f77166f2b4965c4f51e5b59c49347c62f49d0e86
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-06 15:23:09 +0600

    Polishing those book card

--- Milestone_8/Module_47/boipoka/src/pages/Book/Book.jsx
+++ Milestone_8/Module_47/boipoka/src/pages/Book/Book.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { FaStarHalfStroke } from "react-icons/fa6";
+
 
 const Book = ({ singlebook }) => {
     console.log(singlebook);
-    const { image, } = singlebook
-
+    const { image, author, bookId, bookName, publisher, review, totalPages, yearOfPublishing, rating, category } = singlebook
     return (
         <div>
-            <div className="card bg-base-200  shadow-sm">
-                <figure>
-                    <img className='w-3/5 h-[300px]'
+            <div className="card bg-base-200  shadow-sm border">
+                <figure className='p-8 bg-gray-200 w-2/3 mx-auto rounded-xl my-3'>
+                    <img className=' h-[170px]'
                         src={image}
-                        alt="Shoes" />
+                        alt="Book" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Card Title
-                        <div className="badge badge-secondary">NEW</div>
+                        {bookName}
+                        <div className="badge badge-secondary p-4">{author}</div>
                     </h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <p>A book title plays a crucial role in setting the tone and expectations for the reader. To Kill a Mockingbird suggests a deeper</p>
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+                        <div className="badge badge-outline">{category}</div>
+                        <div className="badge badge-outline">{rating}<FaStarHalfStroke /></div>
                     </div>
                 </div>
             </div>
