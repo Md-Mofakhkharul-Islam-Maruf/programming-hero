cae0e9d751c40fa0239b64358e0c410666ee5616
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-07 14:08:13 +0600

    adding sweet alert and toastify

--- Milestone_8/Module_47/boipoka/src/pages/BookDetails/BookDetails.jsx
+++ Milestone_8/Module_47/boipoka/src/pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../utilities/addToDb';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { ToastContainer, toast } from 'react-toastify';
+
+const MySwal = withReactContent(Swal)
 
 const BookDetails = () => {
     const { id } = useParams()
@@ -12,17 +17,27 @@ const BookDetails = () => {
     // console.log(newId, singleBook)
     const { image, author, bookId, bookName, publisher, review, totalPages, yearOfPublishing, rating, category, tags } = singleBook;
 
-    const handleMarkAsRead = (id)=>{
+    const handleMarkAsRead = (id) => {
+
+        Swal.fire({
+            title: "Good Job....",
+            icon: "success",
+            text: 'Mark as read',
+            draggable: true
+        });
+
+        toast("Wow so easy!");
 
         addToStoredDB(id)
+        
     }
 
     return (
         <div className='flex gap-10 justify-center'>
             <div>
                 <img className='h-[400px]' src={image} alt="" />
-                <div className='flex justify-center gap-10 my-5'> 
-                    <button onClick={()=>handleMarkAsRead(id)} className='px-6 py-3 bg-blue-500 text-white rounded'>Mark as read</button>
+                <div className='flex justify-center gap-10 my-5'>
+                    <button onClick={() => handleMarkAsRead(id)} className='px-6 py-3 bg-blue-500 text-white rounded'>Mark as read</button>
                     <button className='px-6 py-3 bg-gray-500 text-white rounded'>Add to Wishlist</button>
                 </div>
             </div>
@@ -33,6 +48,7 @@ const BookDetails = () => {
                 <p>{totalPages}</p>
                 <p>{yearOfPublishing}</p>
                 <p>{tags}</p>
+                <ToastContainer />
             </div>
         </div>
     );
