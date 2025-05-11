e3a2ad62fd10b0335cf3881383e510271df46d5d
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-11 16:42:32 +0600

    auth er kichui bujhi nai ami

--- Milestone_9/Module_52_5_1/dragon-news/src/components/pages/Register.jsx
+++ Milestone_9/Module_52_5_1/dragon-news/src/components/pages/Register.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const Register = () => {
+    const { createUser, setUser } = use(AuthContext)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(e.target)
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log({ name, email, photo, password })
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                // console.log(user);
+                setUser(user)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage)
+                // ..
+            });
+    }
     return (
         <div className='flex justify-center min-h-screen items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <h2 className='text-center font-bold text-2xl mt-5'>Login your account</h2>
-                <div className="card-body">
+                <h2 className='text-center font-bold text-2xl mt-5'>Register your account</h2>
+                <form onSubmit={handleSubmit} className="card-body">
                     <fieldset className="fieldset">
                         <label className="label">Name</label>
-                        <input type="text" className="input" placeholder="Name" />
+                        <input required name="name" type="text" className="input" placeholder="Name" />
                         <label className="label">Photo</label>
-                        <input type="url" className="input" placeholder="Photo url" />
+                        <input required name="photo" type="url" className="input" placeholder="Photo url" />
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input required name="email" type="email" className="input" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
-                        <button className="btn btn-neutral mt-4">Login</button> 
+                        <input required name="password" type="password" className="input" placeholder="Password" />
+                        <button className="btn btn-neutral mt-4" type='submit'>Register</button>
                         <p className='text-center'>Already have an Account?<Link to={'/auth/login'} className=' font-bold text-red-600'> Login</Link></p>
                     </fieldset>
-                </div>  
+                </form>
             </div>
         </div>
     );
--- Milestone_9/Module_52_5_1/dragon-news/src/provider/AuthProvider.jsx
+++ Milestone_9/Module_52_5_1/dragon-news/src/provider/AuthProvider.jsx
@@ -1,13 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
-    const [user, setUser] = useState({
-        name: 'hablu mia',
-        email: 'hablu@mia.com'
-    });
-    const authData = { user, setUser }
+    const [user, setUser] = useState(null
+        // name: 'hablu mia',
+        // email: 'hablu@mia.com'
+
+    );
+    console.log(user)
+
+    const createUser= (email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    useEffect(()=>{
+       const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser)
+        });
+        return()=> unsubscribe();
+    },[])
+    const authData = { user, setUser, createUser }
     return (
         <AuthContext value={authData} >{children}</AuthContext>
     );
