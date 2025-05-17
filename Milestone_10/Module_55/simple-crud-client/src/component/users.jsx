8168ceb2632496cd30cf49a3ce98926db0c66cd1
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-17 11:47:59 +0600

    ready to delte

--- Milestone_10/Module_55/simple-crud-client/src/component/users.jsx
+++ Milestone_10/Module_55/simple-crud-client/src/component/users.jsx
@@ -1,8 +1,9 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
 const Users = ({promise}) => {
     const initialUser = use(promise)
     console.log(initialUser)
+    const [users, setUsers]= useState(initialUser)
 
     
     const handleAddUser = e => {
@@ -24,12 +25,24 @@ const Users = ({promise}) => {
             .then(data => {
                 console.log('Data after creating user in the db', data)
                 if(data.insertedId){
+                    newUser._id = data.insertedId;
+                    const newUsers = [...users, newUser]
+                    setUsers(newUsers)
                     alert('User added successfully')
                     e.target.reset()
                 }
 
             })
+
     }
+
+    const handleDeleteUser = (id)=>{
+        console.log('delete user successfully',id)
+        fetch(`http://localhost:3000/users/${id}`,{
+            method: "DELETE",
+        }).then(res=> res.json()).then(data=>console.log(data))
+    }
+
     return (
         <div>
             <div>
@@ -39,7 +52,18 @@ const Users = ({promise}) => {
                     <input type="submit" value="Add User" /><br />
                 </form>
             </div>
+
+            {/* {Show users} */}
+            <div>
+                {
+                    users.map(user => <p key={user._id}>
+                        {user.name}: {user.email}
+                        <button onClick={()=>handleDeleteUser(user._id)}>X</button>
+                        </p>)
+                }
+            </div>
         </div>
+        
     );
 };
 
