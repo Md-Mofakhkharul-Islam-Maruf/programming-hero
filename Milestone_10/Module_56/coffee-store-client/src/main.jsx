467d288792f7b35cb4acb3ea57d1d6fd393218b4
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-05-18 08:11:41 +0600

    create few component

--- Milestone_10/Module_56/coffee-store-client/src/main.jsx
+++ Milestone_10/Module_56/coffee-store-client/src/main.jsx
@@ -2,15 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './component/Home.jsx'
+import AddCoffee from './component/AddCoffee.jsx'
+import UpdateCoffee from './component/UpdateCoffee.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import MainLayout from './Layouts/MainLayout.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: App
+    Component: MainLayout,
+    children:[
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: '/addCoffee',
+        Component: AddCoffee,
+      },
+      {
+        path: '/UpdateCoffee',
+        Component: UpdateCoffee,
+      }
+    ]
   },
 ]);
 
